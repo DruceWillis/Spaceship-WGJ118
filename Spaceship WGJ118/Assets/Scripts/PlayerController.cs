0984cb351b19091e2db7dab03566deeb0fef0996
DruceWillis <willis.druce@gmail.com>
2019-10-12 11:46:16 +0300

    Added project to git repo

---
+++ Spaceship WGJ118/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Aim();
+    }
+
+    private void Aim()
+    {
+        float addAngle = 270;
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + addAngle;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void Movement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 newPosition = new Vector3(horizontalMovement, verticalMovement, 0);
+        newPosition = newPosition.normalized * speed * Time.deltaTime;
+        transform.position += newPosition;
+    }
+}
---
+++ Spaceship WGJ118/Assets/Scripts/ShootingMechanics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMechanics : MonoBehaviour
+{
+    Rigidbody2D projectile;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject barrel;
+    [SerializeField] float bulletSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject bulletInstance = (GameObject)Instantiate(bullet, barrel.transform.position, transform.rotation);
+            
+            bulletInstance.GetComponent<Rigidbody2D>().velocity = barrel.transform.up * bulletSpeed;
+            Destroy(bulletInstance, 4f);
+        }
+    }
+}
